7f581bc99b283765fdbfe8f495b2398342dc4947
Harrison Burt <hburt2003@gmail.com>
2021-09-28 09:51:09 +0100

    add basic fizz buzz game with extra rule 1

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,33 @@ namespace FizzBuzz
     {
         static void Main(string[] _)
         {
-            
+            Tuple<int, string>[] options =
+            {
+                Tuple.Create(3, "Fizz"),
+                Tuple.Create(5, "Buzz"),
+                Tuple.Create(7, "Bang")
+            };
+
+            for (var i = 1; i <= 100; i++)
+            {
+                var matched = false;
+                foreach (var (value, msg) in options)
+                {
+                    if (i % value == 0)
+                    {
+                        matched = true;
+                        Console.Write(msg);
+                    }
+                }
+
+                if (!matched)
+                {
+                    Console.Write(i);
+                }
+                
+                Console.Write("\n");
+            }
+                
         }
     }
 }
